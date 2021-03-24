e9567777fba2b5c36a0113e7579f39494a9bbf7b
Bryan Gee <begee217@gmail.com>
2021-03-24 13:08:50 -0500

    weather function

--- wthrscript.js
+++ wthrscript.js
@@ -6,14 +6,21 @@
 var apiKey = "3a8117101994cd1cfbb2c9c8f394442f";
 var baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
 // going to use more specific parameter for city/ state request q={city name},{state code}&appid={API key}
-function searchForCityWeather(city, stateCode) {
-  var fullUrl = baseUrl + "q=" + city + "&appid=" + apiKey;
+function searchForCityWeather(city) {
+  var fullUrl = baseUrl + "q=" + city + "&appid=" + apiKey + "&units=imperial";
   fetch(fullUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log(data.main);
+      $("#current-temp").html(
+        `<div class = "currentTemp">${data.main.temp}</div> <div>${data.main.feels_like}</div>`
+      );
     });
 }
-searchForCityWeather("Dallas");
+$("#search-city-form").submit(function (event) {
+  alert("Handler for .submit() called.");
+  event.preventDefault();
+  searchForCityWeather($("input").first().val());
+});
